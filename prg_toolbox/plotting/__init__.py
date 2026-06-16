378e438f8ccb177f2fcbe6f7b42df3d51b2f467e
Damiaom <daniel.miranda.castro@gmail.com>
2026-06-16 12:20:57 +0200

    modified:   prg_toolbox/__init__.py 	modified:   prg_toolbox/plotting/__init__.py

--- prg_toolbox/plotting/__init__.py
+++ prg_toolbox/plotting/__init__.py
@@ -1,22 +1,20 @@
-# from .plot_mean_variance import plot_mean_variance
-# from .plot_log_silence_probability import plot_log_silence_probability
-# from .plot_max_covariance_eigenvalue import plot_max_covariance_eigenvalue
-# from .plot_covariance_spectrum import plot_covariance_spectrum
-# from .plot_autocorrelation_function import plot_autocorrelation_function
-# from .plot_decay_time import plot_decay_time
-# from .plot_activity_distribution import plot_activity_distribution
-# from .plot_avalanche_covariance_eigenvalue import plot_avalanche_covariance_eigenvalue
+from .plot_mean_variance import plot_mean_variance
+from .plot_log_silence_probability import plot_log_silence_probability
+from .plot_max_covariance_eigenvalue import plot_max_covariance_eigenvalue
+from .plot_covariance_spectrum import plot_covariance_spectrum
+from .plot_autocorrelation_function import plot_autocorrelation_function
+from .plot_decay_time import plot_decay_time
+from .plot_activity_distribution import plot_activity_distribution
+from .plot_avalanche_covariance_eigenvalue import plot_avalanche_covariance_eigenvalue
 
-# # Optional: explicitly define what is available publicly in this submodule
-# __all__ = [
-#     "plot_mean_variance",
-#     "plot_log_silence_probability",
-#     "plot_max_covariance_eigenvalue",
-#     "plot_covariance_spectrum",
-#     "plot_autocorrelation_function",
-#     "plot_decay_time",
-#     "plot_activity_distribution",
-#     "plot_avalanche_covariance_eigenvalue"
-# ]
-import numpy as np
-print('hahaha mas ue to rindo a toa')
+# Optional: explicitly define what is available publicly in this submodule
+__all__ = [
+    "plot_mean_variance",
+    "plot_log_silence_probability",
+    "plot_max_covariance_eigenvalue",
+    "plot_covariance_spectrum",
+    "plot_autocorrelation_function",
+    "plot_decay_time",
+    "plot_activity_distribution",
+    "plot_avalanche_covariance_eigenvalue"
+]
